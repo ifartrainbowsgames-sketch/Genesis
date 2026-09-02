b49dde13f750592d46fe235a0bcbc1a3caa280fd
ifartrainbowsgames-sketch <ifartrainbows.games@gmail.com>
2026-09-02 08:09:55 +0700

    Bind Genesis API to loopback by default

--- apps/server/app/config.py
+++ apps/server/app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Genesis"
-    server_host: str = "0.0.0.0"
+    server_host: str = "127.0.0.1"
     server_port: int = 8000
     web_origin: str = "http://localhost:3000"
     workspace_root: str = "./workspace"
